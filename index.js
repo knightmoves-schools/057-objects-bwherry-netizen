6e7577f0ff04ce43100d96cd16416c61cc658d0f
Brendon W. <bwherry@midland.k12.ia.us>
2025-11-18 12:04:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+let school = {
+    'name': 'Burnded',
+    'average class size': 10,
+    'mascot': 'cow',
+}
+
+
+document.getElementById("result").innerHTML = "${school.name} has an average class size of ${school[average class size]} and their mascot is a ${school[mascot]}"
 
